d08809bcd865d6163ff24c294be37c09872c0f27
Barni <166104424+BabuskaBarn@users.noreply.github.com>
2025-12-04 17:26:26 +0100

    Artefacts are now correct

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,8 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
-require("dotenv").config();
-
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Beware: NEVER put real Ether into testing accounts
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-
-// Go to https://infura.io, sign up, create a new API key
-// in its dashboard, and add it to the configuration variables
-const INFURA_API_KEY = process.env.INFURA_API_KEY
-
-
-
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
