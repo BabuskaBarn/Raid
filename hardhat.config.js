16d5b578d9d7907abacc2a7caeb27dbd506004ef
Barni <166104424+BabuskaBarn@users.noreply.github.com>
2025-12-04 18:52:47 +0100

    Artefacts are now correct

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,22 @@
-require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
-
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+
+  // NEU: V2-Config
+  verify: {
+    etherscan: {
+      // dein normaler Etherscan-Key, funktioniert jetzt
+      apiKey: process.env.ETHERSCAN_API_KEY,
     },
   },
 };
